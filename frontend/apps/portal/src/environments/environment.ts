2bf332916100f54c2e0d10aa195fd90498d1c324
OHA2025g <dt692664@gmail.com>
2025-11-05 23:41:56 +0530

    Update environment.ts

--- frontend/apps/portal/src/environments/environment.ts
+++ frontend/apps/portal/src/environments/environment.ts
@@ -1,8 +1,8 @@
 // Development environment
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8100',
-  gatewayUrl: 'http://localhost:8000',
-  frontendUrl: 'http://localhost:4200'
+  apiUrl: 'api.menindata.org',
+  gatewayUrl: 'api.menindata.org',
+  frontendUrl: 'api.menindata.org'
 };
 
