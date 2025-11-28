97670171b192c1fa46c9e825792452cda56a0861
rafiqshaikh94@gmail.com <rafiqshaikh94@gmail.com>
2025-11-28 21:44:42 +0530

    CM-251127: local environment updated

--- frontend/apps/portal/src/environments/environment.ts
+++ frontend/apps/portal/src/environments/environment.ts
@@ -1,8 +1,8 @@
 // Development environment
 export const environment = {
   production: false,
-  apiUrl: 'https://api.menindata.org',
-  gatewayUrl: 'https://api.menindata.org',
-  frontendUrl: 'https://menindata.org'
+  apiUrl: 'https://localhost:3000/api',
+  gatewayUrl: 'https://localhost:3001',
+  frontendUrl: 'https://localhost:4200',
 };
 
